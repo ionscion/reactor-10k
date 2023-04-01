674cca8d00ebec4374ce55672d4b949cef08ff8f
bensboto <bensboto@gmail.com>
2023-03-31 20:25:37 -0600

    added dropdown component and panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import Dropdown from "./components/Dropdown";
-
+import { useState } from "react";
+import AccordionPage from "./pages/AccordionPage"
 
 function App() {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
   const options = [
     { label: "Red", value: "red" },
     { label: "Green", value: "green" },
     { label: "Blue", value: "blue" },
   ];
 
-  return <Dropdown options={options} />;
+  return (
+    <div className="flex">
+      <Dropdown value={selection} onChange={handleSelect} options={options} />
+    </div>
+  );
 }
 
 export default App;
